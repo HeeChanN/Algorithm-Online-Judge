9edeab5e7231d3d944cf9b888daeccc11ba6fcde
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-01-22 14:19:49 +0900

    day6 5문제

--- SQL/day6.sql
+++ SQL/day6.sql
@@ -1,46 +1,49 @@
 
-# 1. 저자 별 카테고리 별 매출액 집계하기
-SELECT b.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, sum(s.SALES * b.PRICE) as TOTAL_SALES
-from BOOK b
-inner join AUTHOR a on
-    b.AUTHOR_ID = a.AUTHOR_ID
-inner join BOOK_SALES s on
-    b.BOOK_ID = s.BOOK_ID
-where s.SALES_DATE between '2022-01-01' and '2022-01-31'
-group by b.AUTHOR_ID, b.CATEGORY
-order by a.AUTHOR_ID, b.CATEGORY desc
+# 1. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기
+SELECT month(r.START_DATE) as MONTH, r.CAR_ID, count(r.HISTORY_ID) as RECORDS
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY r
+where CAR_ID in (
+    select rr.CAR_ID
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY rr
+    where month(rr.START_DATE) in (8,9,10)
+    group by rr.CAR_ID
+    having count(rr.HISTORY_ID) >=5
+) AND MONTH(START_DATE) IN (8,9,10)
+group by MONTH,r.CAR_ID
+having COUNT(HISTORY_ID) >= 1
+order by 1, 2 desc
 
-# 2. 카테고리별 도서 판매량 집계하기
-SELECT b.CATEGORY, sum(bs.SALES)
-from BOOK b
-inner join BOOK_SALES bs on
-    b.BOOK_ID = bs.BOOK_ID
-where bs.SALES_DATE between '2022-01-01' and '2022-01-31'
-group by b.CATEGORY
-order by b.CATEGORY
 
-# 3. 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
- SELECT c.CAR_ID, ifnull(r.AVAILABILTY,'대여 가능') as AVAILABILITY
- from (
-    select cc.HISTORY_ID, cc.CAR_ID, '대여중' as AVAILABILTY
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY cc
-    where cc.START_DATE <= '2022-10-16' and '2022-10-16' <= cc.END_DATE
- ) r
- right outer join  CAR_RENTAL_COMPANY_RENTAL_HISTORY c on 
-    r.CAR_ID = c.CAR_ID
-group by c.CAR_ID
-order by c.CAR_ID desc
+# 2. 성분으로 구분한 아이스크림 총 주문량
+SELECT a.INGREDIENT_TYPE, sum(b.TOTAL_ORDER)
+from ICECREAM_INFO a
+inner join FIRST_HALF b on
+    a.FLAVOR = b.FLAVOR
+group by a.INGREDIENT_TYPE
 
-# 4. 진료과별 총 예약 횟수 출력하기
-SELECT a.MCDP_CD as '진료과코드', count(a.APNT_NO) as '5월예약건수'
-from appointment a
-where a.APNT_YMD like "2022-05-%"
-group by a.MCDP_CD
-order by 2, 1
+# 3. 식품분류별 가장 비싼 식품의 정보 조회하기
+SELECT f.CATEGORY, f.PRICE as MAX_PRICE, f.PRODUCT_NAME
+from FOOD_PRODUCT f
+inner join (
+    select ff.CATEGORY, max(ff.price) as p
+    from FOOD_PRODUCT ff
+    where ff.CATEGORY in ('과자','국','김치','식용유')
+    group by ff.CATEGORY
+) as n on
+    f.PRICE = n.p and f.CATEGORY = n.CATEGORY
+order by 2 desc;
 
-# 5. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
-SELECT c.CAR_TYPE ,count(c.CAR_ID) as CARS
-from CAR_RENTAL_COMPANY_CAR c
-where c.OPTIONS like '%시트%'
-group by c.CAR_TYPE
-order by c.CAR_TYPE
+# 4. 년, 월, 성별 별 상품 구매 회원 수 구하기
+SELECT year(o.SALES_DATE) as YEAR, month(o.SALES_DATE) as MONTH, u.GENDER , count(distinct u.USER_ID) as USERS
+from ONLINE_SALE o
+inner join USER_INFO u on
+    o.USER_ID = u.USER_ID
+WHERE U.GENDER IS NOT NULL
+group by year(o.SALES_DATE), month(o.SALES_DATE), u.GENDER
+order by 1,2,3
+
+# 5. 고양이와 개는 몇 마리 있을까?
+SELECT a.ANIMAL_TYPE, count(a.ANIMAL_ID)
+from ANIMAL_INS a
+group by a.ANIMAL_TYPE
+order by a.ANIMAL_TYPE
