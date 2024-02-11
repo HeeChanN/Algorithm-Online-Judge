6a184180d957fbb7cc07749e7dea790c6d53fcae
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-02-11 17:50:19 +0900

    day10 5문제

--- SQL/day10.sql
+++ SQL/day10.sql
@@ -1,44 +1,30 @@
+1. 루시와 엘라 찾기
+select a.ANIMAL_ID, a.NAME, a.SEX_UPON_INTAKE
+from ANIMAL_INS a
+where a.NAME in ('Lucy', 'Ella' ,'Pickle','Rogan','Sabrina','Mitty')
 
-# 1. 모의고사 1
-SELECT n.ID, n.CREATOR,n.LIKES
-from NEW_POSTS n
+2. 중성화 여부 파악하기
+SELECT a.ANIMAL_ID, a.NAME, CASE WHEN a.SEX_UPON_INTAKE like 'Spayed%' or a.SEX_UPON_INTAKE like 'Neutered%' THEN 'O'
+ELSE 'X'
+END AS '중성화'
+from ANIMAL_INS a
 
-union
+3. 이름에 el이 들어가는 동물 찾기
+SELECT a.ANIMAL_ID, a.NAME
+from ANIMAL_INS a
+where a.NAME like '%el%' and a.ANIMAL_TYPE = 'Dog'
+order by 2
 
-select o.ID, o.CREATOR,0
-from OLD_POSTS o
+4. DATETIME에서 DATE로 형 변환
+SELECT a.ANIMAL_ID, a.NAME, DATE_FORMAT(a.DATETIME, '%Y-%m-%d') as '날짜'
+from ANIMAL_INS a
+order by 1
 
-order by 1 desc
-
-
-# 2. 모의고사 2
-SELECT m.ALGORITHM_TYPE
-from PROBLEMS m
-group by m.ALGORITHM_TYPE
-order by count(m.ID) desc, m.ALGORITHM_TYPE
-limit 1
-
-# 3. 조건에 부합하는 중고거래 상태 조회하기
-SELECT B.BOARD_ID, B.WRITER_ID, B.TITLE, B.PRICE, 
-CASE WHEN B.STATUS ='SALE' then '판매중'
-     when b.STATUS = 'RESERVED' then '예약중'
-     when b.STATUS = 'DONE' then '거래완료'
-     END AS STATUS
-from USED_GOODS_BOARD B
-where B.CREATED_DATE = '2022-10-05'
-order by 1 desc;
-
-
-# 4. 자동차 평균 대여 기간 구하기
-SELECT h.CAR_ID, round(avg(DATEDIFF(h.END_DATE,h.START_DATE)+1),1) as AVERAGE_DURATION
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY h
-group by h.CAR_ID
-having AVERAGE_DURATION >=7
-order by 2 desc, 1 desc
-
-# 5. 카테고리 별 상품 개수 구하기
-SELECT substr(p.PRODUCT_CODE,1,2) AS CATEGORY, count(p.PRODUCT_ID)
-from PRODUCT p
-group by CATEGORY
-order by 1;
-
+5. 자동차 대여 기록에서 장기/단기 대여 구분하기
+SELECT a.HISTORY_ID, a.CAR_ID, DATE_FORMAT(a.START_DATE,"%Y-%m-%d"), DATE_FORMAT(a.END_DATE,"%Y-%m-%d"),
+CASE when (DATEDIFF(a.END_DATE,a.START_DATE) +1) >= 30 then '장기 대여'
+            else '단기 대여'
+            end as RENT_TYPE
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY a
+where a.START_DATE like '2022-09-%'
+order by a.HISTORY_ID desc
