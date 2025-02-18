091080248621685b2c9d949bdc5a540b2d6fde9f
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-02-18 14:03:23 +0900

    돌 게임
    [수학]
    N=1일 때부터 N=6일 때까지 직접 나올 수 있는 경우를 생각해보며 규칙을 찾았다.
    1개 or 3개이기 때문에 N이 짝수 개이면 무조건 상근이가 먼저 시작하므로 어떤 짓을 해도 질 수가 없다.

---
+++ 2025/수학/백준 9655번.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    if(n % 2 == 1){
+        cout << "SK";
+    }
+    else{
+        cout<<"CY";
+    }
+    
+    return 0;
+}
