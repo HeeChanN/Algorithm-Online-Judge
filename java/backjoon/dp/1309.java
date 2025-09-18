49826a731ba2bf5d3d5d4a7f6f66800ceffe8cb9
Ahn Heechan <88177732+HeeChanN@users.noreply.github.com>
2025-09-18 10:16:55 +0900

    백준 1309번 동물원
    dp
    
    이전에 c++로 문제를 해결했을 때는 재귀방식으로 해결하였다. 하지만 자바의 경우 N의 크기가 10만이 넘어가면 힙스택이 부족해 StackOverFlow가 날 수 있기 때문에 무조건 점화식으로 해결해야하는 문제다.
    
    이 문제는 이친수와 비슷하게 해결하였다. dp[n]의 개수를 구하기 위해서는 dp[n-1]의 위치에 사자가 있는 경우와 없는 경우를 구분해야한다.
    따라서 dp[n-2]의 경우의수를 이용해서 n-1의 자리에 사자가 없는 경우를 뽑아내고 그 값에 n의 자리에 올 수 있는 경우의 수인 3을 곱했다.
    n-1의 위치에 사자가 있을 경우에는 이친수 문제와 똑같이 dp[n-1] - dp[n-2]를 한 값에 n의 자리에 사자가 올 수 있는 경우의 수인 2를 곱하여
    최종적으로 다음과 같은 점화식을 만들어서 해결했다.
    
    dp[n] = dp[n-1] * 3 + (dp[n-1] - dp[n-2]) * 2

---
+++ java/backjoon/dp/1309.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    static final int MOD = 9901;
+    
+    static FastScanner fs = new FastScanner();
+    static int n;
+    static int [] dp = new int[100004];
+    
+    public static void main(String[] args) throws Exception{
+        n = fs.nextInt();
+        dp[0] = 1;
+        dp[1] = 3;
+        for(int i = 2;i<=n;i++){
+            dp[i] = (dp[i-2] * 3 + (dp[i-1] - dp[i-2]) * 2) % MOD;
+        }
+        System.out.print(dp[n]);
+    }
+    
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        String next() throws Exception{
+            while(st == null || !st.hasMoreTokens()){
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() throws Exception{
+            return Integer.parseInt(next());
+        }
+    }
+}
