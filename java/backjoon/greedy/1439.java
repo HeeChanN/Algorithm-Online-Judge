3047aac3d854358dc5dc0a22d794900ab324312b
Heechan Ahn <88177732+HeeChanN@users.noreply.github.com>
2026-02-10 21:47:29 +0900

    백준 1439번 뒤집기
    그리디

---
+++ java/backjoon/greedy/1439.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int count0 = 0;
+        int count1 = 0;
+
+        if (s.charAt(0) == '1') {
+            count0++;
+        } else {
+            count1++;
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                if (s.charAt(i + 1) == '1') {
+                    count0++;
+                } 
+  
+                else {
+                    count1++;
+                }
+            }
+        }
+
+        System.out.println(Math.min(count0, count1));
+        
+        sc.close();
+    }
+}
